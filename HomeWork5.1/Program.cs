a7a3fce44fcd83c2e5c913f5b3c058435d15d5d6
Евгений S <vv-bart-vv@tut.by>
2021-03-09 23:29:08 +0300

    Добавьте файлы проекта.

---
+++ HomeWork5.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HomeWork5._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Каждый охотник желает знать где сидит фазан";
+            int result = 0;
+            char[] glasnie = { 'а', 'о', 'и', 'е', 'ё', 'э', 'ы', 'у', 'ю', 'я', 'А', 'О', 'И', 'Е', 'Ё', 'Э', 'Ы', 'У', 'Ю', 'Я' };
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < glasnie.Length; j++)
+                {
+                    if (glasnie[j] == str[i])
+                    {
+                        result++;
+                    }
+                }
+            }
+                                  
+            Console.WriteLine(result);
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == 'а')
+                {
+                    Console.Write('А'); ;
+                }
+                else
+                {
+                    Console.Write(str[i]);
+                }
+            }
+
+            
+        }
+    }
+}
